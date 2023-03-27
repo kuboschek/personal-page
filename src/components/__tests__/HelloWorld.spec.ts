e859d135d85bc03792c2787202512f18b0b8cad7
Leonhard Kuboschek <leo@legartis.ai>
2023-03-27 15:39:02 +0200

    disable pointless test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "vitest";
 
+/*
 import { mount } from "@vue/test-utils";
 import HelloWorld from "../HelloWorld.vue";
 
@@ -9,3 +10,4 @@ describe("HelloWorld", () => {
     expect(wrapper.text()).toContain("Hello Vitest");
   });
 });
+*/
